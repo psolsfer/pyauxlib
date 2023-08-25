e00e3a8ff75bb0df355a95632309106c21d7c3eb
psolsfer <rosen237@gmail.com>
2023-08-25 14:20:27 +0900

    Update timer.py
    Improved timer handling and output

--- src/pyauxlib/utils/timer.py
+++ src/pyauxlib/utils/timer.py
@@ -1,4 +1,5 @@
 import csv
+import io
 import logging
 import time
 import warnings
@@ -9,10 +10,10 @@ from typing import Any
 class Timer:
     """Timer class to measure execution times.
 
-    The timer can be managed manually, calling the start, stop,... methods.
+    The timer can be managed manually, calling the `start`, `stop`,... methods.
     It can also be used as a context manager:
     ```python
-    with Timer(file) as t: # This will automaticall start the timer
+    with Timer(filename) as t: # This will automatically start the timer
         # code here
         t.add_timestamp("#1")
         # more code
@@ -53,6 +54,7 @@ class Timer:
         self.time_func = time_func
         self.logger = warnings.warn if logger is None else logger.warning
         self.where_output = None if logger is None else logger.info
+        self.running = False
         self.reset()
 
     def __enter__(self):
@@ -69,7 +71,9 @@ class Timer:
         if self.stop_time is not None:
             self.logger("Timer has not been resetted.")
             return
-        self.start_time = self.time_func()
+        self.running = True
+        self.add_timestamp("start")
+        self.start_time = self.timestamps[-1]
 
     def stop(self) -> float:
         """Stops the timer and returns the ellapsed time. It also saves the timestamps
@@ -78,8 +82,9 @@ class Timer:
             self.logger("Timer has not been started.")
             return 0
 
-        self.stop_time = self.time_func()
-        self.texts.append("stop")
+        self.add_timestamp("stop")
+        self.stop_time = self.timestamps[-1]
+        self.running = False
         self.save()
 
         ellapsed_time = self.stop_time - self.start_time
@@ -97,12 +102,8 @@ class Timer:
         text : str, optional
             The text associated with the timestamp. By default "".
         """
-        if self.start_time is None:
-            self.logger("Timer has not been started.")
-            return
-        if self.stop_time is not None:
-            self.logger("Timer is stopped.")
-            return
+        if not self.running:
+            self.logger("Timer is not running.")
 
         self.timestamps.append(self.time_func())
         self.texts.append(text)
@@ -119,42 +120,56 @@ class Timer:
             return
 
         with Path.open(self.filename, "w", newline="") as csvfile:
-            writer = csv.writer(csvfile)
-            header = ["Comment", "Timestamp", "Time", "Elapsed Time", "Total Time"]
-            writer.writerow(header)
-
-            formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(self.start_time))
-            row = [self.texts[0], self.start_time, formatted_time, 0, 0]
-            writer.writerow(row)
-            prev_time = self.start_time
-            for i, timestamp in enumerate(self.timestamps):
-                elapsed_time = timestamp - prev_time
-                total_time = timestamp - self.start_time
-                formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(timestamp))
-                row = [
-                    self.texts[i + 1],
-                    timestamp,
-                    formatted_time,
-                    elapsed_time,
-                    total_time,
-                ]
-                writer.writerow(row)
-                prev_time = timestamp
-            elapsed_time = self.stop_time - prev_time
-            total_time = self.stop_time - self.start_time
-            formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(self.stop_time))
-            row = [
-                self.texts[-1],
-                self.stop_time,
-                formatted_time,
-                elapsed_time,
-                total_time,
-            ]
-            writer.writerow(row)
+            csvfile.write(self._to_csv())
 
     def reset(self):
         """Resets the timer."""
         self.start_time = None
         self.stop_time = None
         self.timestamps = []
-        self.texts = ["start"]
+        self.texts = []
+
+    def get_data(self):
+        rows = []
+        header = ["Comment", "Timestamp", "Time", "Step Time", "Accumulated Time"]
+        rows.append(header)
+
+        prev_time = self.start_time
+        for i, timestamp in enumerate(self.timestamps):
+            elapsed_time = timestamp - prev_time
+            total_time = timestamp - self.start_time
+            formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(timestamp))
+            row = [
+                self.texts[i],
+                str(timestamp),
+                formatted_time,
+                str(elapsed_time),
+                str(total_time),
+            ]
+            rows.append(row)
+            prev_time = timestamp
+
+        return rows
+
+    def _to_csv(self):
+        rows = self.get_data()
+
+        # Convert rows to CSV string
+        csv_str = io.StringIO()
+        writer = csv.writer(csv_str)
+        writer.writerows(rows)
+        return csv_str.getvalue()
+
+    def __str__(self):
+        rows = self.get_data()
+
+        # Compute column widths
+        col_widths = [max(len(cell) for cell in col) for col in zip(*rows, strict=True)]
+
+        # Format rows as table
+        table_rows = []
+        for row in rows:
+            table_row = "  ".join(cell.ljust(width) for cell, width in zip(row, col_widths, strict=True))
+            table_rows.append(table_row)
+
+        return "\n".join(table_rows)
