6dc178ce888f790ff37ecbc123d086fabfcf24c5
psolsfer <rosen237@gmail.com>
2023-09-08 18:07:55 +0900

    Update timer.py

--- src/pyauxlib/utils/timer.py
+++ src/pyauxlib/utils/timer.py
@@ -4,7 +4,12 @@ import logging
 import time
 import warnings
 from pathlib import Path
-from typing import Any
+from typing import Any, ClassVar, Protocol, Self
+
+
+class TimeFunc(Protocol):
+    def __call__(self) -> float:
+        ...
 
 
 class Timer:
@@ -24,9 +29,9 @@ class Timer:
     ----------
     filename : str | Path | None
         The name or path of the file to save the timestamps.
-    time_func : callable, optional
-        The time function to use. It can be time.time, time.perf_counter or
-        time.process_time. By default time.time.
+    time_func : {"time", "perf_counter", "process_time"} | callable, optional
+        The time function to use. It can be one of {"time", "perf_counter",
+        "process_time"} or a custom time function. By default "time".
     logger : logging.Logger, optional
         The logger to use for logging warnings. By default None.
 
@@ -44,20 +49,31 @@ class Timer:
         The list of texts associated with each timestamp.
     """
 
+    TIME_FUNC: ClassVar[dict[str, TimeFunc]] = {
+        "time": time.time,
+        "perf_counter": time.perf_counter,
+        "process_time": time.process_time,
+    }
+
     def __init__(
         self,
         filename: str | Path | None = None,
-        time_func=time.time,
+        time_func: str | TimeFunc = "time",
         logger: logging.Logger | None = None,
     ):
-        self.filename = filename
-        self.time_func = time_func
+        self.filename = Path(filename) if filename is not None else None
+
+        if isinstance(time_func, str):
+            self.time_func = self.TIME_FUNC[time_func]
+        else:
+            self.time_func = time_func
+
         self.logger = warnings.warn if logger is None else logger.warning
         self.where_output = None if logger is None else logger.info
         self.running = False
         self.reset()
 
-    def __enter__(self):
+    def __enter__(self) -> Self:
         """Start a new timer as a context manager"""
         self.start()
         return self
@@ -108,18 +124,27 @@ class Timer:
         self.timestamps.append(self.time_func())
         self.texts.append(text)
 
-    def save(self):
-        """Saves the timestamps and their associated texts to a file."""
+    def save(self, filename: str | Path | None = None):
+        """Saves the timestamps and their associated texts to a file.
+
+        Parameters
+        ----------
+        filename : str | Path | None, optional
+            The name or path of the file to save the timestamps to. If not provided,
+            the filename attribute of the Timer object will be used. By default None.
+        """
         if self.start_time is None:
             self.logger("Timer has not been started.")
             return
         if self.stop_time is None:
             self.logger("Timer has not been stopped yet.")
             return
-        if self.filename is None:
+        if (filename := filename or self.filename) is None:
             return
 
-        with Path.open(self.filename, "w", newline="") as csvfile:
+        filename = Path(filename)
+
+        with Path.open(filename, "w", newline="") as csvfile:
             csvfile.write(self._to_csv())
 
     def reset(self):
