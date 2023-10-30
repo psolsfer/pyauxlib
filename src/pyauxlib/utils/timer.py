6fec4a51e5df0b7b8c583ca150d3472a1b3270ec
psolsfer <23430901+psolsfer@users.noreply.github.com>
2023-10-30 15:30:45 +0900

    feat: improve usage of timer
    Added elapsed_time, a helper for the usage of Timer as decorator, new tests and improved docstrings

--- src/pyauxlib/utils/timer.py
+++ src/pyauxlib/utils/timer.py
@@ -10,9 +10,9 @@ from pathlib import Path
 import wrapt
 
 try:
-    from typing import Any, ClassVar, Protocol, Self  # Only works from Python 3.11
+    from typing import Any, ClassVar, Literal, Protocol, Self  # Only works from Python 3.11
 except ImportError:
-    from typing import Any, ClassVar, Protocol
+    from typing import Any, ClassVar, Literal, Protocol
 
     from typing_extensions import Self
 
@@ -39,7 +39,8 @@ class Timer:
     1. Manually, by calling the `start`, `stop`, etc. methods.
     2. As a context manager, which automatically starts the timer when entering the context and
     stops it when exiting.
-    3. As a decorator, which times the execution of the decorated function.
+    3. As a decorator, which times the execution of the decorated function. The helper function
+    'pyauxlib.decorators.timer' simplifies the use as decorator (see the example below).
 
     Parameters
     ----------
@@ -49,7 +50,7 @@ class Timer:
         The time function to use. It can be one of {"time", "perf_counter",
         "process_time"} or a custom time function. By default "time".
     logger : logging.Logger, optional
-        The logger to use for logging warnings. By default None.
+        The logger to use for logging warnings and elapsed time. By default None.
 
     Attributes
     ----------
@@ -59,10 +60,33 @@ class Timer:
         The start time of the timer.
     stop_time : float or None
         The stop time of the timer.
+    elapsed_time : float or None
+        Total elapsed time (stop_time - start_time)
     timestamps : list of float
         The list of timestamps added during the timer execution.
     texts : list of str
         The list of texts associated with each timestamp.
+
+    Methods
+    -------
+    start():
+        Start the timer.
+
+    stop():
+        Stop the timer and returns the elapsed time. It also saves the timestamps to a file, when
+        provided.
+
+    add_timestamp(text):
+        Add a timestamp with an associated text.
+
+    save(filename):
+        Save the timestamps and their associated texts to a file.
+
+    reset():
+        Reset the timer.
+
+    get_data():
+        Get the timer data as a list of rows.
 
     Examples
     --------
@@ -87,6 +111,13 @@ class Timer:
     @timer
     def some_function():
         # function code
+
+    # Decorator usage with 'timer' helper function
+    from pyauxlib.decorators.timer import timer
+     @timer(filename='timer.txt')
+     def some_function():
+         # function code
+
     ```
     """
 
@@ -99,7 +130,7 @@ class Timer:
     def __init__(
         self,
         filename: str | Path | None = None,
-        time_func: str | TimeFunc = "time",
+        time_func: Literal["time", "perf_counter", "process_time"] | TimeFunc = "time",
         logger: logging.Logger | None = None,
     ):
         self.filename = Path(filename) if filename is not None else None
@@ -134,7 +165,9 @@ class Timer:
         """Measure the execution time of a decorated function or method."""
         self.start()
         result = wrapped(*args, **kwargs)
-        self.stop()
+        elapsed_time = self.stop()
+        if self.logger is not None:
+            self.logger.info("Elapsed time: %f s", elapsed_time)
         return result
 
     def start(self) -> None:
@@ -147,7 +180,7 @@ class Timer:
         self.start_time = self.timestamps[-1]
 
     def stop(self) -> float:
-        """Stop the timer and returns the ellapsed time.
+        """Stop the timer and returns the elapsed time.
 
         It also saves the timestamps to a file, when provided.
         """
@@ -159,13 +192,13 @@ class Timer:
         self.stop_time = self.timestamps[-1]
         self.running = False
         self.save()
-
-        ellapsed_time = self.stop_time - self.start_time
+        elapsed_time = self.stop_time - self.start_time
+        self.elapsed_time = elapsed_time
 
         if self.where_output is not None:
-            self.where_output(f"Ellapsed time: {ellapsed_time} s")
+            self.where_output(f"Elapsed time: {elapsed_time} s")
 
-        return ellapsed_time
+        return elapsed_time
 
     def add_timestamp(self, text: str = "") -> None:
         """Add a timestamp with an associated text.
@@ -209,6 +242,7 @@ class Timer:
         with contextlib.suppress(AttributeError):
             del self.start_time
             del self.stop_time
+            del self.elapsed_time
         self.timestamps: list[float] = []
         self.texts: list[str] = []
 
@@ -271,3 +305,16 @@ class Timer:
             table_rows.append(table_row)
 
         return "\n".join(table_rows)
+
+    def __getattr__(self, name: str) -> Any:
+        """Return the given attribute."""
+        raise_error: str = ""
+        if name == "start_time":
+            raise_error = f"{name} is not set. Please start the timer first."
+        elif name in {"stop_time", "elapsed_time"}:
+            if hasattr(self, "start_time"):
+                raise_error = f"{name} is not set. Please stop the timer first."
+            else:
+                raise_error = f"{name} is not set. Please start the timer first."
+        if raise_error:
+            raise AttributeError(raise_error)
